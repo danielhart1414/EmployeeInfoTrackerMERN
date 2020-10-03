ea0ce90f6487d76e55c30991f238c05613961a69
danielhart1414 <daniel_hart@live.com>
2020-10-03 13:43:42 -0700

    Installed concurrently & bootstrap; made header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
+import Header from './components/Header';
 
 function App() {
     return (
         <div>
+            <Header />
             <Router>
                 {/* <Route path="/" exact component={TodosList} />
                 <Route path="/edit/:id" component={EditTodo} /> */}
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navbar, Container } from 'react-bootstrap';
+
+const Header = () => {
+    return(
+        <header>
+            <Navbar fixed="top" className="navbar-dark bg-info border-bottom box-shadow mb-3">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <h1>EmployeeData</h1>
+                    </Navbar.Brand>
+                </Container>
+            </Navbar>
+        </header>
+    );
+}
+
+export default Header;
