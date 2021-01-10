751b5ad81f1162748e286f90394773914d4b5417
danielhart1414 <daniel_hart@live.com>
2021-01-09 19:05:23 -0500

    Polished get, post, and delete routes

--- backend/employeeRoutes.js
+++ backend/employeeRoutes.js
@@ -7,15 +7,13 @@ router.post('/', (req, res, next) => {
     Employee.findOne({ id: req.body.id })
         .then(user => {
             if (user) {
-                res.status(409).json({ existingUser: "Employee already exists"});
+                res.status(409).json({ existingEntry: "Employee already exists"});
             } else {
                 Employee.create(req.body, (error, data) => {
-                    if (error) {
+                    if (error)
                         return next(error);
-                    } else {
-                        console.log(data);
+                    else 
                         res.status(200).json(data);
-                    }
                 });
             }
         })
@@ -24,12 +22,10 @@ router.post('/', (req, res, next) => {
 
 router.get('/', (req, res) => {
     Employee.find((error, data) => {
-        if (error) {
+        if (error)
             return next(error);
-        } else {
-            console.log(data);
+        else
             res.status(200).json(data);
-        }
     });
 });
 
@@ -44,12 +40,11 @@ router.route('/:id').put((req, res, next) => {
 });
 
 router.route('/:id').delete((req, res, next) => {
-    Employee.findByIdAndRemove(req.params.id, (error, data) => {
-        if (error) {
+    Employee.deleteOne({ id: req.params.id }, (error, data) => {
+        if (error)
             return next(error);
-        } else {
-            res.status(200).send("Employee successfully deleted!");
-        }
+        else
+            res.status(200).send('Deletion successful');
     });
 });
 
